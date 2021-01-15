4737ef8e0afa4e4016daad811de0b7cffa63d1e7
Marlon Mascarenhas <mmascarenhs@gmail.com>
2021-01-14 21:23:01 -0300

    criacao da API

--- backend/ClubinhoDoBebe.API/Program.cs
+++ backend/ClubinhoDoBebe.API/Program.cs
@@ -1,9 +1,26 @@
-﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace ClubinhoDoBebe.API {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+namespace ClubinhoDoBebe.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
